d42d5941276488ee23c4c6318332e8f61633bf89
Malte Ubl <malte.ubl@gmail.com>
2011-01-27 20:33:28 +0100

    Remove the actual dependency on a global var in the code. Leave it in for debugging in the console.

--- public/lib/stream/background.js
+++ public/lib/stream/background.js
@@ -5,17 +5,22 @@
 require.def("stream/background",
   function() {
     
+    var m = {};
+    
     if(location.search.indexOf('background=true') != -1) {
       $(document).bind("streamie:init:complete", function () {
         window.Streamie_Loaded = true;
-      })
+      });
+      $(function () {
+        $('body').hide(); // no need to *actually* render anything. Just need the HTML.
+      });
     } else {
       if(window.chrome.app.isInstalled) {
         var bg = window.open('/?background=true#yah', 'background', 'background');
-        window.Background = bg;
+        window.Background = m.page = bg;
       }
     }
     
-    return {}
+    return m;
   }
 );
--- public/lib/stream/initplugins.js
+++ public/lib/stream/initplugins.js
@@ -3,8 +3,8 @@
  */
 
 require.def("stream/initplugins",
-  ["stream/tweet", "stream/settings", "stream/twitterRestAPI", "stream/helpers", "text!../templates/tweet.ejs.html", "ext/cookie.js"],
-  function(tweetModule, settings, rest, helpers, templateText) {
+  ["stream/tweet", "stream/settings", "stream/background", "stream/twitterRestAPI", "stream/helpers", "text!../templates/tweet.ejs.html", "ext/cookie.js"],
+  function(tweetModule, settings, background, rest, helpers, templateText) {
     
     settings.registerNamespace("general", "General");
     settings.registerKey("general", "showTwitterBackground", "Show my background from Twitter",  false);
@@ -242,7 +242,7 @@ require.def("stream/initplugins",
       prefillTimeline: {
         func: function prefillTimeline (stream) { 
           
-          var bg = window.Background;
+          var bg = background.page;
           if(bg && bg.Streamie_Loaded) {
             $('#stream').html( bg.$('#stream').html() );
             $(document).trigger("tweet:first");
